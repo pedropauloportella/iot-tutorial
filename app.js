59b6df36890d8b241785d89fd4f0c27b5006aa63
bmarchete <bmarchete@gmail.com>
2017-07-19 02:21:15 -0300

    alterações da aula2

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var board = new five.Board();
 
 board.on("ready", function() {
   var led = new five.Led(13);
-
+  var rele = new five.Relay(8);
   // This will grant access to the led instance
   // from within the REPL that's created when
   // running this program.
   this.repl.inject({
-    led: led
+    led: led,
+    rele : rele
   });
 
-  led.toggle();
 });
