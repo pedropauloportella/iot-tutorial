e7f126ffca2f18e1c0fee93d87b1ed0a419f207f
bmarchete <bmarchete@gmail.com>
2017-07-19 16:04:28 -0300

    alterações aula3

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 var five = require("johnny-five");
+var firebase = require("firebase");
+
 var board = new five.Board();
 
 board.on("ready", function() {
-  var led = new five.Led(13);
   var rele = new five.Relay(8);
-  // This will grant access to the led instance
-  // from within the REPL that's created when
-  // running this program.
+  
   this.repl.inject({
-    led: led,
     rele : rele
+  });
+
+  // Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "AIzaSyCjzkTVhEkv2E5Z5YozoSIpjpdj9M5L9C4",
+    authDomain: "iot-tutorial-31c2e.firebaseapp.com",
+    databaseURL: "https://iot-tutorial-31c2e.firebaseio.com",
+    storageBucket: "iot-tutorial-31c2e.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  var starCountRef = firebase.database().ref('lampada').on('value', function(snapshot) {
+    let lampada = snapshot.val();
+
+    if (lampada == 'on') {
+      rele.on();
+    } else {
+      rele.off();
+    }
   });
 
 });
